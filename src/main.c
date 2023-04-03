cf930aab6726ad710bfcba9bc86ab57988a26e09
simone stanganini <simonestanganini@simones-MacBook-Pro.local>
2023-04-03 10:54:01 +0200

    fixed mutlicast control

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include <pcap/pcap.h> 
+#include <pcap/pcap.h>
 #include <signal.h>
 #include <sched.h>
 #include <stdlib.h>
@@ -8,7 +8,7 @@
 
 #define DEFAULT_SNAPLEN 256
 
-pcap_t  *pd;
+pcap_t *pd;
 int verbose = 0;
 struct pcap_stat pcapStats;
 
@@ -24,10 +24,10 @@ struct pcap_stat pcapStats;
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-//#include <netinet/ip6.h>
-#include <net/ethernet.h>     /* the L2 protocols */
+// #include <netinet/ip6.h>
+#include <net/ethernet.h> /* the L2 protocols */
 
-//static struct timeval startTime;
+// static struct timeval startTime;
 unsigned long long numPkts = 0, numBytes = 0;
 
 #include <ifaddrs.h>
@@ -41,7 +41,8 @@ struct sockaddr_in maxMultiIP;
 
 /*************************************************/
 
-char* __intoa(unsigned int addr, char* buf, u_short bufLen) {
+char *__intoa(unsigned int addr, char *buf, u_short bufLen)
+{
     char *cp, *retStr;
     u_int byte;
     int n;
@@ -50,11 +51,13 @@ char* __intoa(unsigned int addr, char* buf, u_short bufLen) {
     *--cp = '\0';
 
     n = 4;
-    do {
+    do
+    {
         byte = addr & 0xff;
         *--cp = byte % 10 + '0';
         byte /= 10;
-        if (byte > 0) {
+        if (byte > 0)
+        {
             *--cp = byte % 10 + '0';
             byte /= 10;
             if (byte > 0)
@@ -65,38 +68,48 @@ char* __intoa(unsigned int addr, char* buf, u_short bufLen) {
     } while (--n > 0);
 
     /* Convert the string to lowercase */
-    retStr = (char*)(cp+1);
+    retStr = (char *)(cp + 1);
 
-    return(retStr);
+    return (retStr);
 }
 
 /* ************************************ */
 
 static char buf[sizeof "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"];
-char* intoa(unsigned int addr) {
-    return(__intoa(addr, buf, sizeof(buf)));
+char *intoa(unsigned int addr)
+{
+    return (__intoa(addr, buf, sizeof(buf)));
 }
 /* ******************************** */
 
-void sigproc(int sig) {
+void sigproc(int sig)
+{
     static int called = 0;
     fprintf(stderr, "Leaving...\n");
-    if (called) return; else called = 1;
+    if (called)
+        return;
+    else
+        called = 1;
     pcap_breakloop(pd);
 }
 
 /* *************************************** */
 
-char* proto2str(u_short proto) {
+char *proto2str(u_short proto)
+{
     static char protoName[8];
 
-    switch(proto) {
-        case IPPROTO_TCP:  return("TCP");
-        case IPPROTO_UDP:  return("UDP");
-        case IPPROTO_ICMP: return("ICMP");
-        default:
-               snprintf(protoName, sizeof(protoName), "%d", proto);
-               return(protoName);
+    switch (proto)
+    {
+    case IPPROTO_TCP:
+        return ("TCP");
+    case IPPROTO_UDP:
+        return ("UDP");
+    case IPPROTO_ICMP:
+        return ("ICMP");
+    default:
+        snprintf(protoName, sizeof(protoName), "%d", proto);
+        return (protoName);
     }
 }
 
@@ -104,96 +117,122 @@ char* proto2str(u_short proto) {
 
 ENTRY s, d, *sp, *dp;
 
-typedef struct data{
+typedef struct data
+{
     int src;
     int dsc;
     struct timeval t;
-}DATA;
+} DATA;
 
-long delta_time (struct timeval * now, struct timeval * before) {
-  time_t delta_seconds;
-  time_t delta_microseconds;
-  delta_seconds      = now -> tv_sec  - before -> tv_sec;
-  delta_microseconds = now -> tv_usec - before -> tv_usec;
-  if(delta_microseconds < 0) {
-    delta_microseconds += 1000000;  /* 1e6 */
-    -- delta_seconds;
-  }
-  return((delta_seconds * 1000000) + delta_microseconds);
+long delta_time(struct timeval *now, struct timeval *before)
+{
+    time_t delta_seconds;
+    time_t delta_microseconds;
+    delta_seconds = now->tv_sec - before->tv_sec;
+    delta_microseconds = now->tv_usec - before->tv_usec;
+    if (delta_microseconds < 0)
+    {
+        delta_microseconds += 1000000; /* 1e6 */
+        --delta_seconds;
+    }
+    return ((delta_seconds * 1000000) + delta_microseconds);
 }
 
-void dummyProcesssPacket(u_char *_deviceId, const struct pcap_pkthdr *h, const u_char *p) {
-  struct ether_header ehdr;
-  struct ip ip;
-  struct tcphdr tcp;
-  struct udphdr udp;
+void dummyProcesssPacket(u_char *_deviceId, const struct pcap_pkthdr *h, const u_char *p)
+{
+    struct ether_header ehdr;
+    struct ip ip;
+    struct tcphdr tcp;
+    struct udphdr udp;
 
-  DATA *ipaddr;
+    DATA *ipaddr;
 
-  memcpy(&ehdr, p, sizeof(struct ether_header));
-  memcpy(&ip, p + sizeof(ehdr), sizeof(struct ip));
-  u_short eth_type = ntohs(ehdr.ether_type);
+    memcpy(&ehdr, p, sizeof(struct ether_header));
+    memcpy(&ip, p + sizeof(ehdr), sizeof(struct ip));
+    u_short eth_type = ntohs(ehdr.ether_type);
 
-  if (eth_type == 0x0800) {
-    printf("SrcIP: %-15s", intoa(ntohl(ip.ip_src.s_addr)));
-    printf(" | DstIP: %-15s", intoa(ntohl(ip.ip_dst.s_addr)));
-    printf(" | P: %-5s", proto2str(ip.ip_p));
-    if (ip.ip_p == IPPROTO_TCP) {
-        memcpy(&tcp, p + sizeof(ehdr) + sizeof(ip), sizeof(struct tcphdr));
-        printf(" | SrcP: %-10u", tcp.th_sport);
-        printf(" | DstP: %-10u", tcp.th_dport);
-    } else if (ip.ip_p == IPPROTO_UDP) {
-        memcpy(&udp, p + sizeof(ehdr) + sizeof(ip), sizeof(struct udphdr));
-        printf(" | SrcP: %-10u", udp.uh_sport);
-        printf(" | DstP: %-10u", udp.uh_dport);
-    }
+    if (eth_type == 0x0800)
+    {
+        if (ntohl(ip.ip_dst.s_addr) != ntohl(broadcastIP.sin_addr.s_addr) &&
+            (ntohl(ip.ip_dst.s_addr) < ntohl(minMultiIP.sin_addr.s_addr) || ntohl(ip.ip_dst.s_addr) > ntohl(maxMultiIP.sin_addr.s_addr)))
+        {
+            printf("SrcIP: %-15s", intoa(ntohl(ip.ip_src.s_addr)));
+            printf(" | DstIP: %-15s", intoa(ntohl(ip.ip_dst.s_addr)));
+            printf(" | P: %-5s", proto2str(ip.ip_p));
+            if (ip.ip_p == IPPROTO_TCP)
+            {
+                memcpy(&tcp, p + sizeof(ehdr) + sizeof(ip), sizeof(struct tcphdr));
+                printf(" | SrcP: %-10u", tcp.th_sport);
+                printf(" | DstP: %-10u", tcp.th_dport);
+            }
+            else if (ip.ip_p == IPPROTO_UDP)
+            {
+                memcpy(&udp, p + sizeof(ehdr) + sizeof(ip), sizeof(struct udphdr));
+                printf(" | SrcP: %-10u", udp.uh_sport);
+                printf(" | DstP: %-10u", udp.uh_dport);
+            }
+            printf("\n");
 
-    s.key = intoa(ntohl(ip.ip_src.s_addr));
-    sp = hsearch(s, FIND);
-    if (sp == NULL) {
-        ipaddr = malloc(sizeof(DATA));
-        ipaddr->src = 1;
-        ipaddr->dsc = 0;
-        s.data = (void *)ipaddr;
-        hsearch(s, ENTER);
-        printf("inserito src\n");
-    } else {
-        DATA *a = sp->data;
-        a->src = 1;
-        printf("già presente src\n");
-    }
+            // IP Sorgente
+            s.key = intoa(ntohl(ip.ip_src.s_addr));
+            sp = hsearch(s, FIND);
+            if (sp == NULL)
+            {
+                ipaddr = malloc(sizeof(DATA));
+                ipaddr->src = 1;
+                ipaddr->dsc = 0;
+                s.data = (void *)ipaddr;
+                hsearch(s, ENTER);
+                printf("inserito src\n");
+            }
+            else
+            {
+                DATA *a = sp->data;
+                a->src = 1;
+                printf("già presente src\n");
+            }
 
-    d.key = intoa(ntohl(ip.ip_dst.s_addr));
-    dp = hsearch(s, FIND);
-    if (dp == NULL) {
-        ipaddr = malloc(sizeof(DATA));
-        ipaddr->src = 0;
-        ipaddr->dsc = 1;
-        ipaddr->t = h->ts;
-        // ipaddr->t = &startTime;
-        d.data = (void *)ipaddr;
-        printf("ts prima %lu\n", ipaddr->t.tv_usec);
-        dp = hsearch(s, ENTER);
-        printf("inserito dsc\n");
-    } else {
-        DATA *a = dp->data;
-
-        a->dsc = 1;
-        if (!(a->src)) {
-            printf("ts dopo %lu\n", a->t.tv_usec);
+            // IP Destinatario
+            d.key = intoa(ntohl(ip.ip_dst.s_addr));
+            dp = hsearch(s, FIND);
+            if (dp == NULL)
+            {
+                ipaddr = malloc(sizeof(DATA));
+                ipaddr->src = 0;
+                ipaddr->dsc = 1;
+                ipaddr->t = h->ts;
+                d.data = (void *)ipaddr;
+                printf("TS PRIMA %d\n", ipaddr->t.tv_usec);
+                dp = hsearch(s, ENTER);
+                printf("inserito dsc\n");
+            }
+            else
+            {
+                DATA *a = (DATA *)dp->data;
+                a->dsc = 1;
+                if (!(a->src))
+                {
+                    printf("TS DOPO %d\n", a->t.tv_usec);
+                }
+                printf("già presente dsc\n");
+            }
         }
-        printf("già presente dsc\n");
+        else
+        {
+            printf("BROADCAST\n");
+        }
     }
-  } else {
-    printf("dk");
-  }
-  printf("\n");
+    else
+    {
+        // printf("dk");
+    }
 }
 
 /* *************************************** */
 
-//struct sockaddr_in* getSubnetMask(char* device) {
-void getBroadCast(char* device) {
+// struct sockaddr_in* getSubnetMask(char* device) {
+void getBroadCast(char *device)
+{
     struct ifaddrs *ifaddr, *ifa;
     struct sockaddr_in *sa = NULL;
     struct sockaddr_in *su = NULL;
@@ -201,11 +240,17 @@ void getBroadCast(char* device) {
     char ip[INET_ADDRSTRLEN];
     char broad[INET_ADDRSTRLEN];
 
-    if (getifaddrs(&ifaddr) == -1) {perror("getifaddrs");exit(EXIT_FAILURE);}
-    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
-        if (strcmp(ifa->ifa_name, device) == 0 && ifa->ifa_addr->sa_family == AF_INET) {
-            sa = (struct sockaddr_in *) ifa->ifa_netmask;
-            su = (struct sockaddr_in *) ifa->ifa_addr;
+    if (getifaddrs(&ifaddr) == -1)
+    {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (strcmp(ifa->ifa_name, device) == 0 && ifa->ifa_addr->sa_family == AF_INET)
+        {
+            sa = (struct sockaddr_in *)ifa->ifa_netmask;
+            su = (struct sockaddr_in *)ifa->ifa_addr;
             inet_ntop(AF_INET, &(sa->sin_addr), subnet_mask, INET_ADDRSTRLEN);
             inet_ntop(AF_INET, &(su->sin_addr), ip, INET_ADDRSTRLEN);
             break;
@@ -213,53 +258,83 @@ void getBroadCast(char* device) {
     }
 
     broadcastIP.sin_addr.s_addr = su->sin_addr.s_addr | ~(sa->sin_addr.s_addr);
+    printf("broadcast PRIMA: %u\n", broadcastIP.sin_addr.s_addr);
+    printf("broadcast con ntohl: %u\n", ntohl(broadcastIP.sin_addr.s_addr));
+    printf("MIO: %s\n", intoa(ntohl(broadcastIP.sin_addr.s_addr)));
     inet_ntop(AF_INET, &(broadcastIP.sin_addr), broad, INET_ADDRSTRLEN);
     printf("SubnetMask:%s localIP:%s broadcastIP: %s\n", subnet_mask, ip, broad);
     freeifaddrs(ifaddr);
 }
 
-void getMultiCast() {
-    inet_pton(AF_INET,"224.0.0.0",&minMultiIP.sin_addr);
-    inet_pton(AF_INET,"239.255.255.255",&maxMultiIP.sin_addr);
+void getMultiCast()
+{
+    inet_pton(AF_INET, "224.0.0.0", &minMultiIP.sin_addr);
+    inet_pton(AF_INET, "239.255.255.255", &maxMultiIP.sin_addr);
 }
 
 /* *************************************** */
 
-int main(int argc, char* argv[]) {
-    char *device = NULL; 
+int main(int argc, char *argv[])
+{
+    char *device = NULL;
     u_char c;
     size_t n = 10000;
     char errbuf[PCAP_ERRBUF_SIZE];
     int promisc, snaplen = DEFAULT_SNAPLEN;
 
-    while((c = getopt(argc, argv, "hi:l:v:f:")) != '?') {
-        if((c == 255) || (c == (u_char)-1)) break;
-        switch(c) {
-            case 'i':
-                device = strdup(optarg);
-                break;            
+    while ((c = getopt(argc, argv, "hi:l:v:f:")) != '?')
+    {
+        if ((c == 255) || (c == (u_char)-1))
+            break;
+        switch (c)
+        {
+        case 'i':
+            device = strdup(optarg);
+            break;
         }
     }
 
-    if(geteuid() != 0) {
+    if (geteuid() != 0)
+    {
         printf("Please run this tool as superuser\n");
-        return(-1);
+        return (-1);
     }
 
-    if(device == NULL) {
-        printf("ERROR: Missing -i\n");    
-        return(-1);  
+    if (device == NULL)
+    {
+        printf("ERROR: Missing -i\n");
+        return (-1);
     }
     printf("Capturing from %s\n", device);
     getBroadCast(device);
     getMultiCast();
 
+    //  min multicasdt 3758096384
+    //  broad cast 11000000101010000111111111111111 4286556352
+    //  192.168.127.255 11000000101010000111111111111111
+    //  255      127      168      192
+    //  11111111 01111111 10101000 11000000  = E' al CONTRARIOOOOOOOOO
+    //  00000000 00000000 00000000 11111111
+    //struct sockaddr_in prova;
+    //inet_pton(AF_INET, "224.0.0.251", &prova.sin_addr);
+    //printf("myIP %u\nminmulti %u\nmaxmulti %u\nbroadcast %u\n", prova.sin_addr.s_addr, ntohl(minMultiIP.sin_addr.s_addr), ntohl(maxMultiIP.sin_addr.s_addr), broadcastIP.sin_addr.s_addr);
+    //if (ntohl(prova.sin_addr.s_addr) < ntohl(minMultiIP.sin_addr.s_addr) || ntohl(prova.sin_addr.s_addr) > ntohl(maxMultiIP.sin_addr.s_addr))
+    //{
+    //    printf("NON MUTLI\n");
+    //}
+    //else
+    //{
+    //    printf("MUTLI\n");
+    //}
+    //return 0;
+
     /* hardcode: promisc=1, to_ms=500 */
     promisc = 1;
-      
-    if((pd = pcap_open_live(device, snaplen,promisc, 500, errbuf)) == NULL) {
-        printf("pcap_open_live: %s\n", errbuf); 
-        return(-1);
+
+    if ((pd = pcap_open_live(device, snaplen, promisc, 500, errbuf)) == NULL)
+    {
+        printf("pcap_open_live: %s\n", errbuf);
+        return (-1);
     }
     signal(SIGINT, sigproc);
     signal(SIGTERM, sigproc);
@@ -269,5 +344,5 @@ int main(int argc, char* argv[]) {
     hdestroy();
 
     pcap_close(pd);
-    return(0);
+    return (0);
 }
